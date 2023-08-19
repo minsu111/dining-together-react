87e6c0492583764abacbe2c70b380f53c739855c
[SW5] 이수민 <tnklr1111@gmail.com>
2023-08-19 11:07:39 +0900

    Feat: 라우터 추가

--- src/pages/reservation/StoreDetail.tsx
+++ src/pages/reservation/StoreDetail.tsx
@@ -4,13 +4,15 @@ import MiddleButtonWhite from '../../components/common/MiddleButtonWhite';
 import MiddleButtonYellow from '../../components/common/MiddleButtonYellow';
 import Calendar from '../../components/common/Calendar';
 
-function RestaurantDetail() {
-    return <section>
-        <MainButton value="예약하기"/>
-        <MiddleButtonWhite value='이전'/>
-        <MiddleButtonYellow value= '다음' />
-        <Calendar />
-    </section>;
+function StoreDetail() {
+    return (
+        <section>
+            <MainButton value="예약하기" />
+            <MiddleButtonWhite value="이전" />
+            <MiddleButtonYellow value="다음" />
+            <Calendar />
+        </section>
+    );
 }
 
-export default RestaurantDetail;
+export default StoreDetail;

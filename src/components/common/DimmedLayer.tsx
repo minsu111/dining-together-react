2972979df7088f1d30ae21e1fff4be3689b70f62
[SW5] 이수민 <tnklr1111@gmail.com>
2023-08-19 10:28:32 +0900

    Feat: Dimmed창 추가
    - 이외 package 파일 변경사항 적용(캘린더)
    - eslint 파일 확장자 변경

---
+++ src/components/common/DimmedLayer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { styled } from 'styled-components';
+
+function DimmedLayer() {
+    return <Layer />;
+}
+
+export default DimmedLayer;
+
+const Layer = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    background-color: #000;
+    opacity: 44%;
+`;

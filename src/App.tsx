c948d29deb256f8e2bc9c43143c37a8d4c85aaaf
[SW5] 이수민 <tnklr1111@gmail.com>
2023-08-23 01:12:07 +0900

    Fix: GlobalStyle 재적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Reset } from 'styled-reset';
+import GlobalStyle from './styles/globalStyle';
 
 function App() {
     return (
         <div>
             <Reset />
+            <GlobalStyle />
             <Outlet />
         </div>
     );

e240e597b08fb7bbe9a1faa8c0390b0cd5467294
[SW5] 박수현 <pshyeon0707@gmail.com>
2023-08-27 08:56:01 +0900

    feat: 홈 화면 상단 캐러셀, 가로 스크롤, API 연동

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { styled } from 'styled-components';
+import {ScrollMenu} from 'react-horizontal-scrolling-menu';
+import 'react-horizontal-scrolling-menu/dist/styles.css';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import GNBArea from '../../components/common/GNB';
 import Logo from '../../assets/logo.svg';
 import BigImageSample from '../../assets/ImageSampleB.svg'
 import SmallImageSmaple from '../../assets/ImageSampleS.svg'
+import axiosRequest from '../../api/api';
+
 
 function Home() {
+    const [storeList, setStoreList] = useState<Record<string, any>>({});
+    useEffect(()=>{
+        const fetch = async () => {
+            try {
+                setStoreList(await axiosRequest('GET', '/home'));
+            } catch (error) {
+                // 여기에 에러 처리
+                console.log('에러');
+            }
+        }
+        fetch();
+    }, []);
+
     return(
     <Section>
         <TopBar>
             <img alt="" src={Logo}/>
         </TopBar>
-        <img alt="" src={BigImageSample}/>
-        <RecommendList title="건대/성수/왕십리 회식 명소"/>
-        <RecommendList title="이런 장소 어때요?"/>
-        <RecommendList title="30인 이상 단체 가능!"/>
-        <RecommendList title="새로 입점했어요"/>
+        <Slider autoplay speed={1000} infinite pauseOnHover>
+            <img alt="" src={BigImageSample}/>
+            <img alt="" src={BigImageSample}/>
+            <img alt="" src={BigImageSample}/>
+        </Slider>
+        {Object.keys(storeList).map((value, index) => {
+        return(
+        <RecommendList title={value} storeList={storeList}/>
+        )})}
         <GNBArea />
+        
     </Section>
-    ) 
+    )
 }
 
 type StoreCardProps={
@@ -29,7 +54,7 @@ type StoreCardProps={
     foodType: string;
 }
 
-const StoreCard: React.FC<StoreCardProps> = (props) => {
+const StoreCard = (props: StoreCardProps) => {
     const Image = styled.img`
         width: 124px;
         height: 124px;
@@ -38,34 +63,29 @@ const StoreCard: React.FC<StoreCardProps> = (props) => {
     const Card = styled.div`
         width: 124px;
         height: 172px;
-
+        margin-left: 10px;
         h5{
             font-size: 14px;
         }
-
         p{
             font-size: 12px;
         }
     `
     return(
         <Card>
-                <Image alt="" src={props.imgUrl}/>
-                <h5>{props.storeName}</h5>
-                <p>{props.minCount} ~ {props.maxCount}명 · {props.foodType}</p>
+            <Image alt="" src={props.imgUrl}/>
+            <h5>{props.storeName}</h5>
+            <p>{props.minCount} ~ {props.maxCount}명 · {props.foodType}</p>
         </Card>
     )
 }
 
 type RecommendListProps={
     title: string;
+    storeList: Record<string, any>;
 }
 
-const RecommendList:React.FC<RecommendListProps> = (props) =>{
-    const List = styled.div`
-        width: 650px;
-        display: flex;
-        justify-content: space-between;
-    `
+const RecommendList = (props: RecommendListProps) =>{
     const Title = styled.h3`
         font-size: 20px;
         font-weight: bold;
@@ -74,17 +94,16 @@ const RecommendList:React.FC<RecommendListProps> = (props) =>{
         margin: 30px 0 0 20px;
     `
 
-    // 가로 스크롤
     return (
         <Container>
             <Title>{props.title}</Title>
-            <List>
-                <StoreCard imgUrl={SmallImageSmaple} storeName='더즌 오이스터' minCount={10} maxCount={40} foodType='일식'/>
-                <StoreCard imgUrl={SmallImageSmaple} storeName='더즌 오이스터' minCount={10} maxCount={40} foodType='일식'/>
-                <StoreCard imgUrl={SmallImageSmaple} storeName='더즌 오이스터' minCount={10} maxCount={40} foodType='일식'/>
-                <StoreCard imgUrl={SmallImageSmaple} storeName='더즌 오이스터' minCount={10} maxCount={40} foodType='일식'/>
-                <StoreCard imgUrl={SmallImageSmaple} storeName='더즌 오이스터' minCount={10} maxCount={40} foodType='일식'/>
-            </List>
+            <ScrollMenu>
+                {props.storeList[props.title].map((value: Record<string, any>)=>{
+                        return(
+                            <StoreCard imgUrl={SmallImageSmaple} storeName={value.storeId} minCount={10} maxCount={40} foodType={value.foodType}/>
+                        )
+                    })}
+            </ScrollMenu>   
         </Container>
     )
 }
@@ -107,10 +126,6 @@ const Section = styled.section`
     border: 1px solid #e8e8e8;
 `;
 const TopBar = styled.div`
-    width: 390px;
-    height: 50px;
-    padding: 10px 0;
-    border-bottom: 1px solid grey;
     display: flex;
     align-items: center;
     justify-content: center;

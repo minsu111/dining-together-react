15e6a971e9513a10581e7674f4a0dedcaaa6cb1b
[SW5] 이수민 <tnklr1111@gmail.com>
2023-08-29 16:27:55 +0900

    Feat: redux-persist 추가
    - 새로고침 시 데이터 유지

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,34 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
 import { userSlice } from './UserSlice';
 
-export const store = configureStore({
-    reducer: {
-        user: userSlice.reducer,
-    },
+// export const store = configureStore({
+//     reducer: {
+//         user: userSlice.reducer,
+//     },
+// });
+
+const reducers = combineReducers({
+    user: userSlice.reducer,
 });
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: [thunk],
+});
+
+export default store;
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
